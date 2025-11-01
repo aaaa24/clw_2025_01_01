488f12283f00c3220bfbf594dc7d2fdae2d0f5f6
aaaa24 <chernov.arseniy@yandex.ru>
2025-11-01 13:01:03 +0300

    add create and destroy matrix

--- main.cpp
+++ main.cpp
@@ -1,2 +1,43 @@
+#include <iostream>
+
+void destroy(int ** mtx, size_t created)
+{
+  for (size_t i = 0; i < created; i++) {
+    delete[] mtx[i];
+  }
+  delete[] mtx;
+}
+
+int ** create(size_t rows, size_t cols)
+{
+  size_t created = 0;
+  int ** mtx = new int * [rows];
+  try {
+    for (; created < rows; created++) {
+      mtx[created] = new int[cols];
+    }
+  } catch (const std::bad_alloc & e) {
+    destroy(mtx, created);
+    throw;
+  }
+  return mtx;
+}
+
 int main()
-{}
+{
+  size_t rows = 5, cols = 6;
+  int ** matrix = nullptr;
+  try {
+    matrix = create(rows, cols);
+  } catch (const std::bad_alloc & e) {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+  for (size_t i = 0; i < rows; ++i) {
+    std::cout << matrix[i][0];
+    for (size_t j = 1; j < cols; ++j) {
+      std::cout << " " << matrix[i][j];
+    }
+    std::cout << "\n";
+  }
+}
